633a2775c4d8b4091affe2b1e1f4922e6b2c227f
Mads <madsp@hotmail.dk>
2023-02-07 14:25:06 +0100

    redid somethings

---
+++ src/dk/easv/presentation/controller/MovieLayoutController.java
@@ -0,0 +1,22 @@
+package dk.easv.presentation.controller;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MovieLayoutController implements Initializable {
+    public TextArea text1,text2,text3,text4;
+    public Label title1,title2,title3,title4;
+    public ImageView image1,image2,image3,image4;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
