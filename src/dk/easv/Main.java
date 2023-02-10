adc6a0fa02fada20dd091d5ea89b914e288db963
Madsp15 <madsp@hotmail.dk>
2023-02-10 12:10:19 +0100

    made login screen better added Material FX lib

--- src/dk/easv/Main.java
+++ src/dk/easv/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("presentation/view/MovieLayout.fxml"));
-        primaryStage.setTitle("Movie Recommendation System 0.01 Beta");
+        primaryStage.setTitle("Movie Recommendation System 0.5 Beta");
         // primaryStage.setFullScreen(true);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
