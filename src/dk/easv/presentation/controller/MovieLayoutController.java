3d2a79ce40b65373fcc9bf9d2c61de0edae4c9b2
Mads <madsp@hotmail.dk>
2023-02-13 12:43:38 +0100

    new menu

--- src/dk/easv/presentation/controller/MovieLayoutController.java
+++ src/dk/easv/presentation/controller/MovieLayoutController.java
@@ -2,9 +2,7 @@ package dk.easv.presentation.controller;
 
 import dk.easv.presentation.model.AppModel;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Circle;
 
@@ -15,6 +13,7 @@ public class MovieLayoutController implements Initializable {
     public TextArea text1,text2,text3,text4, text5;
     public Label title1,title2,title3,title4, title5;
     public ImageView image1,image2,image3,image4, image5;
+
     public Circle ratingOne1,ratingOne2,ratingOne3,ratingOne4,ratingOne5,ratingOne6,ratingOne7,ratingOne8,ratingOne9,ratingOne10;
 
     public Circle ratingTwo1,ratingTwo2,ratingTwo3,ratingTwo4,ratingTwo5,ratingTwo6,ratingTwo7,ratingTwo8,ratingTwo9,ratingTwo10;
@@ -25,8 +24,8 @@ public class MovieLayoutController implements Initializable {
 
     public Circle ratingFive1,ratingFive2,ratingFive3,ratingFive4,ratingFive5,ratingFive6,ratingFive7,ratingFive8,ratingFive9,ratingFive10;
     public Label lableUser;
-    public Menu menuHome,menuRecommended,menuDiscover, menuWatchAgain,menuRandomMovies, menuLogOut;
-
+    public ToggleButton menuHome,menuRecommended,menuDiscover, menuWatchAgain,menuRandomMovies, menuLogOut;
+    public ToggleGroup MenuGroup;
 
 
     @Override
